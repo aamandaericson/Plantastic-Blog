3bac7afc3cb518eb4cfd2cf5bc2de9e0b9d6c69c
aamandaericson <>
2023-10-13 18:03:30 +0000

    Add files for connection to database.

--- plantastic/settings.py
+++ plantastic/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,12 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5@vslm&tvgg@a0^28b&=(n6j9n*a@@if$7h-_wwmd2!p17n^+e'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-aamandaericson-plantasti-6tx90o0gzu.us2.codeanyapp.com']
+ALLOWED_HOSTS = ['8000-aamandaericson-plantasti-uvqunp4xhv.us2.codeanyapp.com']
 
 
 # Application definition
@@ -74,12 +79,16 @@ WSGI_APPLICATION = 'plantastic.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+ }
 
 
 # Password validation
